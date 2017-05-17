bb6cf3b1b46b401bb8e8b1e70aaf3e96b88eccda
Jason Kroll <jkroll@alumni.tufts.edu>
2017-05-17 09:27:46 -0700

    Add clojure 1.8.0 to profiles.clj deps
    Also stashing tmux.conf because why not it's just comments

--- profiles.clj
+++ profiles.clj
@@ -2,4 +2,5 @@
                   [refactor-nrepl "2.2.0"]
                   [lein-kibit "0.1.3"]]
         :dependencies [[alembic "0.3.2"]
+                       [org.clojure/clojure "1.8.0"]  ;; what the heck
                        [org.clojure/tools.nrepl "0.2.12"]]}}
